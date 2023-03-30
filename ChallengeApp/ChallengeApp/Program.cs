954a02b7b52c256e2274d0b0b0cb31f95e005e2b
Serg <serg.albokrinov@gmail.com>
2023-03-30 19:58:30 +0200

    Zadanie domowe 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string numberInString = Console.ReadLine();
+        var letters = numberInString.ToArray();
+        var numbers = new int[10];
+
+        for(int i = 0; i < letters.Length; i++)
+        {
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                if (letters[i] - 48 == j)
+                {
+                    numbers[j]++;
+                }
+
+            }
+        }
+
+        Console.WriteLine("Wyniki dla liczby " + numberInString + ":");
+        for(int i = 0;i < numbers.Length; i++)
+        {
+            Console.WriteLine($"{i} => {numbers[i]}");
+        }
+    }
+}
